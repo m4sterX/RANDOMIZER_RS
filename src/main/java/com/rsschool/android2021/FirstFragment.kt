af1cf334c2dd2b77ebe6bbb0e6a27788b37c6aef
m4sterX <56063985+m4sterX@users.noreply.github.com>
2021-05-27 12:22:50 +0300

    Add files via upload

---
+++ src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -0,0 +1,61 @@
+package com.rsschool.android2021
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment() {
+
+    private var generateButton: Button? = null
+    private var previousResult: TextView? = null
+    private lateinit var minValue: EditText
+    private lateinit var maxValue: EditText
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        previousResult = view.findViewById(R.id.previous_result)
+        generateButton = view.findViewById(R.id.generate)
+        minValue = view.findViewById(R.id.min_value)
+        maxValue = view.findViewById(R.id.max_value)
+
+
+        val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
+        previousResult?.text = "Previous result: ${result.toString()}"
+
+        generateButton?.setOnClickListener {
+            val min = minValue.text.toString()
+            val max = maxValue.text.toString()
+            //(activity as MainActivity).openSecondFragment(min.toInt(), max.toInt())
+            (activity as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SecondFragment.newInstance(min.toInt(),max.toInt()))
+                .commit()
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(previousResult: Int): FirstFragment {
+            val fragment = FirstFragment()
+            val args = Bundle()
+            args.putInt(PREVIOUS_RESULT_KEY, previousResult)
+            fragment.arguments = args
+            return fragment
+        }
+
+        private const val PREVIOUS_RESULT_KEY = "PREVIOUS_RESULT"
+    }
+}
---
+++ src/main/java/com/rsschool/android2021/SecondFragment.kt
@@ -0,0 +1,64 @@
+package com.rsschool.android2021
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import kotlin.math.nextUp
+
+class SecondFragment : Fragment() {
+
+    private var backButton: Button? = null
+    private var result: TextView? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        result = view.findViewById(R.id.result)
+        backButton = view.findViewById(R.id.back)
+
+        val min = arguments?.getInt(MIN_VALUE_KEY) ?: 0
+        val max = arguments?.getInt(MAX_VALUE_KEY) ?: 0
+
+        result?.text = generate(min, max).toString()
+        val res = result?.text.toString().toInt()
+
+        backButton?.setOnClickListener {
+            (activity as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.container, FirstFragment.newInstance(res))
+                .commit()
+        }
+    }
+
+    private fun generate(min: Int, max: Int): Int {
+        return (min..max).random()
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(min: Int, max: Int): SecondFragment {
+
+            val fragment = SecondFragment()
+            val args = Bundle()
+            args.putInt(MAX_VALUE_KEY, max)
+            args.putInt(MIN_VALUE_KEY, min)
+            fragment.arguments = args
+
+            return fragment
+        }
+
+        private const val MIN_VALUE_KEY = "MIN_VALUE"
+        private const val MAX_VALUE_KEY = "MAX_VALUE"
+    }
+}
